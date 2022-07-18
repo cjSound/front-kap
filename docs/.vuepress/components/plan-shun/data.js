56e5e35b6507a8d92174334889e16e5e21687860
cjSound <876304802@qq.com>
2022-07-19 00:03:29 +0800

    add

--- docs/.vuepress/components/plan-shun/data.js
+++ docs/.vuepress/components/plan-shun/data.js
@@ -2,12 +2,13 @@
  * @Author: 曹捷
  * @Date: 2022-07-17 20:11:01
  * @LastEditors: 曹捷
- * @LastEditTime: 2022-07-17 21:57:28
+ * @LastEditTime: 2022-07-18 23:24:42
  * @Description: 日学习时间
  */
 export const studyList = [
     { date: '7-14', time: 3 },
     { date: '7-15', time: 4.5 },
     { date: '7-16', time: 0 },
+    { date: '7-18', time: 7.25 },
     // { date: '7-18', time: 8 },
 ]
